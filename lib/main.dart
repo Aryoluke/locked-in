9e8da0bd09bc82a57235c9c7297689733103de5e
Aryoluke <175420581+Aryoluke@users.noreply.github.com>
2026-08-28 23:12:22 +0100

    feat: scaffold LOCKED IN mini Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'app.dart';
+import 'providers/app_state.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(ChangeNotifierProvider(
+    create: (_) => AppState()..load(),
+    child: const LockedInApp(),
+  ));
+}
