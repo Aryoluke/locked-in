ea1fb2507eb815258dc3f5be302e74a5bd8fb5d4
Aryoluke <175420581+Aryoluke@users.noreply.github.com>
2026-08-28 23:12:25 +0100

    feat: scaffold LOCKED IN mini Flutter app

---
+++ lib/models/habit.dart
@@ -0,0 +1,9 @@
+class HabitDefinition {
+  const HabitDefinition({required this.id, required this.title, required this.subtitle, required this.icon, required this.xp});
+
+  final String id;
+  final String title;
+  final String subtitle;
+  final String icon;
+  final int xp;
+}
